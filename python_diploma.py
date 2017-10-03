8996c42ca2ccf00aaef8cf910b8fdf1528b836ef
SerjKanunnikov <sergey.kanunnikov@gmail.com>
2017-10-03 10:56:27 +0300

    working intersection, comments

--- python_diploma.py
+++ python_diploma.py
@@ -8,6 +8,7 @@ VERSION = "5.68"
 
 
 def get_friends_list():
+    """Получение списка друзей пользователя"""
     params = {
         "user_id": USER_ID,
         "fields": "first_name",
@@ -23,18 +24,22 @@ def get_friends_list():
 
 
 def get_user_groups():
+    """Получение множества групп пользователя"""
     params = {
         "user_id": USER_ID,
         "access_token": API_TOKEN,
         "v": VERSION
     }
     response = requests.get("https://api.vk.com/method/groups.get", params)
-    user_groups = response.json()
+    user_groups = response.json().get("response").get("items")
+    print(user_groups)
     user_groups_set = set(user_groups)
+    print(user_groups_set)
     return user_groups_set
 
 
 def get_friends_groups(friends_ids):
+    """Получение групп друзей пользователя"""
     groups_of_user_friends = []
     for friend_id in friends_ids:
         params = {
@@ -50,17 +55,17 @@ def get_friends_groups(friends_ids):
 
 
 def get_ids_of_friends_groups(groups_of_user_friends):
-    groups_list = []
+    """Получение множества идентификаторов групп друзей"""
+    friends_groups_set = set()
     for friend in groups_of_user_friends:
-        print(friend["response"]["items"])
-        groups_list.append(set(friend["response"]["items"]))
-    # groups_set = set(groups_list)
-    print(groups_list)
-    return groups_list
+        # print(friend["response"]["items"])
+        friends_groups_set.update(friend["response"]["items"])
+    print(friends_groups_set)
+    return friends_groups_set
 
 
-def intersect_groups(user_groups, groups_list):
-    spy_games = user_groups & groups_list
+def intersect_groups(user_groups_set, friends_groups_set):
+    spy_games = user_groups_set & friends_groups_set
     print(spy_games)
 
 # def export_json():
@@ -68,6 +73,3 @@ def intersect_groups(user_groups, groups_list):
 
 if __name__ == "__main__":
     intersect_groups(get_user_groups(), get_ids_of_friends_groups(get_friends_groups(get_friends_list())))
-
-    # get_groups(get_friends_list())
-    # get_user_groups()
